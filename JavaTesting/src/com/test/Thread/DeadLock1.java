034a3cbd9f422391ccc844a6aeba35ea525e9fbe
SandeepnGithub <SandynGithub@gmail.com>
2018-02-17 14:23:30 +0530

    Create DeadLock1.java

---
+++ JavaTesting/src/com/test/Thread/DeadLock1.java
@@ -0,0 +1,64 @@
+/*
+  
+   Case with a dead lock :
+   
+    Since last method is  synchronised  
+    
+    Class A will  have to wait for object lock of b
+    Class B will have to wait for object lock  of a
+
+
+
+*/
+
+public class DeadLock1 extends Thread{
+      A a=new A();
+      B b=new B();
+    public static void main(String args[]) {
+      DeadLock1 deadLock=new DeadLock1();
+      deadLock.m1();
+    }
+    
+    public void m1(){
+      this.start();//Child thread creation
+      a.display(b);//Main Thread Executing the method
+    }
+    public void run(){
+      b.display(a);//Child thread executing the method
+    }
+}
+class A{
+    public synchronized void display(B b){
+     System.out.println("Inside the A display"); 
+        try{
+            Thread.sleep(5000);
+            System.out.println("Waited for 5 seconds in A");
+        }catch(Exception ex){
+            
+        }
+        System.out.println("A class calling b last method");
+        b.last();
+    }
+    
+    public synchronized  void last(){
+        System.out.println("Inside the A last");
+    }
+}
+
+class B{
+    public synchronized void display(A a){
+        System.out.println("Inside the B display");
+        try{
+            Thread.sleep(5000);
+            System.out.println("Waited for 5 seconds in B");
+        }catch(Exception ex){
+            
+        }
+        System.out.println("B class calling A last method");
+        a.last();
+    }
+    
+    public synchronized void last(){
+        System.out.println("Inside the B last");
+    }
+}
