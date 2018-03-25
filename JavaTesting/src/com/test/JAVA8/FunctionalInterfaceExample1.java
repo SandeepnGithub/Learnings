5d8665814af0e4a09b01da0d5b044c4f2656d052
SandeepnGithub <SandynGithub@gmail.com>
2018-03-25 13:24:45 +0530

    Functional Interface Example1

--- JavaTesting/src/com/test/JAVA8/FunctionalInterfaceExample1.java
+++ JavaTesting/src/com/test/JAVA8/FunctionalInterfaceExample1.java
@@ -3,8 +3,12 @@ package com.test.JAVA8;
 /*******
 FuntionalInterface 
 FuntionalInterface is an interface which can contain only one abstract method and can contain any number of default or static or class methods like (equals,hashcode only definition)
-******/
 
+Output:
+_________________________________________________________
+Display from Interface
+
+******/
 @FunctionalInterface
 interface FI{
 public void display();
@@ -12,7 +16,7 @@ public void display();
 
 public class FunctionalInterfaceExample1 implements FI{
     public static void main(String args[]){
-        FI fi=new FunctionalInterfaceExample();
+        FI fi=new FunctionalInterfaceExample1();
         fi.display();
     }
     public void display(){
